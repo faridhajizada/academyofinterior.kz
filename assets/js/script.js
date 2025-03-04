1c8953a662fbe67e9ed6bef5327595f8eab59033
Hajizada Farid <faridmh@code.edu.az>
2025-03-05 02:05:54 +0400

    Refactor mobile menu and responsive styles

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,3 @@
 document.getElementById("mobile-menu").addEventListener("click", function () {
   document.querySelector(".nav-links").classList.toggle("active");
 });
-
-// document.addEventListener("DOMContentLoaded", () => {
-//   const items = document.querySelectorAll(".accordion-item");
-
-//   items.forEach((item) => {
-//     item.addEventListener("click", () => {
-//       items.forEach((i) => i.classList.remove("active"));
-//       item.classList.add("active");
-//     });
-//   });
-// });
